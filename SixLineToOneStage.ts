03687cdea2ab4c94f284fbeabb83f501ee19da86
Anwesh43 <anweshthecool0@gmail.com>
2018-11-16 19:46:19 +0530

    adding SixLineToOne class

--- SixLineToOneStage.ts
+++ SixLineToOneStage.ts
@@ -150,3 +150,26 @@ class SLONode {
         return this
     }
 }
+
+class SixLineToOne {
+    curr : SLONode = new SLONode(0)
+
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () =>{
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
